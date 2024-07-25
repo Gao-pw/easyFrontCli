5e6bcbb6c259da8db7167edf323f61fcc934430b
siroi <siroi@qq.com>
2024-07-25 20:40:34 +0800

    init commit

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'EasyFront',
+			social: {
+				github: 'https://github.com/maxwell-Zhai/EasyFront_Code',
+			},
+			sidebar: [
+				{
+					label: '开始使用',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'so Easy!', slug: 'guides/example' },
+					],
+				},
+			],
+		}),
+	],
+});
