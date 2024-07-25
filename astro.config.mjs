98b1360e5ad32955ad0512db6e4cb83525ae85ad
siroi <siroi@qq.com>
2024-07-25 21:21:34 +0800

    add sth

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://gao-pw.github.io",
+	base: "easyFrontCli",
 	integrations: [
 		starlight({
 			title: 'EasyFront',
